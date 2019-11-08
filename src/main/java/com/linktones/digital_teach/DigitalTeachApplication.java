df772f48d919f232bb1453c49b1f51e016c6cf07
mapleKirito <myzhouye@gmail.com>
2019-11-09 01:57:19 +0800

    创建仓库

---
+++ src/main/java/com/linktones/digital_teach/DigitalTeachApplication.java
@@ -0,0 +1,13 @@
+package com.linktones.digital_teach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTeachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalTeachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linktones/digital_teach/DigitalTeachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktones.digital_teach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalTeachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
